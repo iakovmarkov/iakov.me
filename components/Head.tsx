f476cb5b7dcc471a8bbaed623de56d3e85b4144e
Iakov <markov@iakov.me>
2020-07-09 23:22:27 +0200

    feat: add meta properties to Head

--- components/Head.tsx
+++ components/Head.tsx
@@ -2,6 +2,9 @@ import NextHead from "next/head";
 import { FunctionComponent } from "react";
 
 const TITLE = "iakov.me";
+const DESCRIPTION =
+  "My personal island on the interweb. Resume, blog and a showcase of my technical abilities.";
+const IMAGE = "https://iakov.me/profile/avatar.jpg";
 
 interface HeadProps {
   title?: string;
@@ -14,6 +17,16 @@ const Head: FunctionComponent<HeadProps> = ({ title }) => (
   <NextHead>
     <title>{getTitle(title)}</title>
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+    <meta name="description" content={DESCRIPTION} />
+    <meta property="og:title" content={getTitle(title)} />
+    <meta property="og:url" content="https://iakov.me/" />
+    <meta property="og:image" content={IMAGE} />
+    <meta property="og:description" content={DESCRIPTION} />
+
+    <meta itemprop="name" content={getTitle(title)} />
+    <meta itemprop="description" content={DESCRIPTION} />
+    <meta itemprop="image" content={IMAGE} />
   </NextHead>
 );
 
